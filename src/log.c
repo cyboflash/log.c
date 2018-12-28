3fbe9c0c58b5e2f5baed0f5423c358fb27acc679
Rail Shafigulin <rshafigulin@rshafigulin-fedora-pc0pvgat.DHCP.thefacebook.com>
2018-12-28 10:51:18 -0800

    Bug fix. Accidentally used strftime instead of snprintf.

--- src/log.c
+++ src/log.c
@@ -108,7 +108,7 @@ void log_log(int level, const char *file, int line, const char *fmt, ...) {
     char buf[27];
     size_t n;
     n = strftime(buf, sizeof(buf), "%F %T", lt);
-    strftime(&buf[n], sizeof(buf)-n, ".%06ld", tv.tv_usec);
+    sprintf(&buf[n], sizeof(buf)-n, ".%06ld", tv.tv_usec);
     buf[sizeof(buf)-1] = '\0';
 #ifdef LOG_USE_COLOR
     fprintf(
